f278eab0678a994a435e84339d7edae3834cebf6
fyodorovv <v.fyodorov@list.ru>
2023-05-26 18:03:38 +0300

    Update hw2.sql

--- hw2.sql
+++ hw2.sql
@@ -5,7 +5,7 @@ USE hw2;
 
 CREATE TABLE sales
 (
-	id INT PRIMARY KEY AUTO_INCREMENT,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     order_date DATE NOT NULL,
     count_product INT NOT NULL
 );
@@ -22,7 +22,7 @@ VALUES
 
 SELECT id,
     CASE
-		WHEN count_product < 100 THEN "меньше 100"
+	WHEN count_product < 100 THEN "меньше 100"
         WHEN count_product BETWEEN 100 AND 300 THEN "100-300"
         ELSE "больше 300"
     END AS "Тип заказа"
@@ -31,13 +31,12 @@ FROM sales;
 
 -- 3. Создайте таблицу “orders”, заполните ее значениями. Покажите “полный” статус заказа, используя оператор CASE
 
-
 CREATE TABLE orders
 (
-	orderid INT PRIMARY KEY AUTO_INCREMENT,
+    orderid INT PRIMARY KEY AUTO_INCREMENT,
     employeeid VARCHAR(20),
     amount FLOAT DEFAULT 0.0,
-	orderstatus VARCHAR(20)
+    orderstatus VARCHAR(20)
 );
 
 INSERT INTO orders (employeeid, amount, orderstatus)
@@ -49,9 +48,9 @@ VALUES
 ('e04', 9.50, "CANCELLED");
 
 SELECT orderid, orderstatus,
-	CASE
-		WHEN orderstatus = "OPEN" THEN "Order is in open state"
-        WHEN orderstatus = "CLOSED" THEN "Order is closed"
+    CASE
+	WHEN orderstatus = "OPEN" THEN "Order is in open state"
+	WHEN orderstatus = "CLOSED" THEN "Order is closed"
         WHEN orderstatus = "CANCELLED" THEN "Order is cancelled"
         ELSE ""
     END AS ordersummary
