1f24adb2859fd0742b6e7def67936444b8ae2d3e
fyodorovv <v.fyodorov@list.ru>
2023-06-05 17:38:29 +0300

    Add files via upload

---
+++ hw4_2.sql
@@ -0,0 +1,20 @@
+use hw4;
+-- Даны 2 таблицы, созданные следующим образом:
+create table test_a (id int, data varchar(1));
+create table test_b (id int);
+insert into test_a(id, data) values
+(10, 'A'),
+(20, 'A'),
+(30, 'F'),
+(40, 'D'),
+(50, 'C');
+insert into test_b(id) values
+(10),
+(30),
+(50);
+
+-- Напишите запрос, который вернет строки из таблицы test_a, id которых нет в таблице test_b, НЕ используя ключевого слова NOT.
+SELECT test_a.*
+FROM test_a
+LEFT JOIN test_b ON test_a.id = test_b.id
+WHERE test_b.id IS NULL;
